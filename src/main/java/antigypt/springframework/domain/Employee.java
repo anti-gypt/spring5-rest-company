e48f795c0bf702273cf530544a30e497f41a3fcb
omid-joukar <joukaromid6@gmail.com>
2021-02-10 08:57:35 +0100

    create employee pojo. Closes #30

---
+++ src/main/java/antigypt/springframework/domain/Employee.java
@@ -0,0 +1,83 @@
+package antigypt.springframework.domain;
+
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="Employee")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "birthDate")
+    private LocalDate birthDate;
+    @Column(name = "hireDate")
+    private LocalDate hireDate;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "homePhone")
+    private String homePhone;
+    @Column(name = "mobilePhone")
+    private String mobilePhone;
+    @Column(name = "detail")
+    private String detail;
+
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride( name = "addressLine", column = @Column(name = "addressLine")),
+            @AttributeOverride( name = "city", column = @Column(name = "city")),
+            @AttributeOverride( name = "region", column = @Column(name = "region")),
+            @AttributeOverride( name = "postalCode", column = @Column(name = "postalCode")),
+            @AttributeOverride( name = "country", column = @Column(name = "country"))
+    })
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private Title title;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Lob
+    @Column(name = "photo")
+    private byte[] photo;
+
+    @OneToMany(mappedBy = "reportsTo", cascade = CascadeType.ALL)
+    private List<Employee> employeeList = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "reportsTo")
+    private Employee reportsTo;
+    @ManyToOne
+    @JoinColumn(name = "jobTitleId")
+    private JobTitle jobTitle;
+    @OneToOne
+    @JoinColumn(name = "employeeSalaryId")
+    private EmployeeSalary employeeSalary;
+    @ManyToOne
+    @JoinColumn(name = "departmentId")
+    private Department department;
+    @OneToOne
+    @JoinColumn(name = "vehicleId")
+    private Vehicle vehicle;
+
+   // @OneToMany(mappedBy = "employee",cascade = CascadeType.ALL )
+   // private List<EmployeeShift> employeeShiftList = new ArrayList<>();
+   // @OneToMany(mappedBy = "employee" , cascade = CascadeType.ALL)
+   // private List<ComplainSuggestion> complainSuggestionList = new ArrayList<>();
+   // @OneToMany(mappedBy = "deliverEmployee",cascade = CascadeType.ALL)
+   // private List<Order> orderList = new ArrayList<>();
+
+}
