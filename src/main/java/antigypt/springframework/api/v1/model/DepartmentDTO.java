4ce9d3e8b4c8539822a64b07ebb4cbc200c734a4
omid-joukar <joukaromid6@gmail.com>
2021-02-11 09:35:42 +0100

    added department json model. Closes #34

---
+++ src/main/java/antigypt/springframework/api/v1/model/DepartmentDTO.java
@@ -0,0 +1,30 @@
+package antigypt.springframework.api.v1.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+
+@Data
+public class DepartmentDTO {
+    @Min(6)
+    private String phoneNumber;
+    @Email
+    private String email;
+    private String detail;
+    @NotBlank
+    private String addressLine;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String region;
+    @NotBlank
+    private String postalCode;
+    @NotBlank
+    private String country;
+    @JsonProperty("depratment_url")
+    private String DepartmetnUrl;
+    private EmployeeListDTO employees;
+}
---
+++ src/main/java/antigypt/springframework/api/v1/model/VehicleDTO.java
@@ -0,0 +1,32 @@
+package antigypt.springframework.api.v1.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleDTO {
+
+    private String vehicleModel;
+    @Past
+    private LocalDate yearOfMake;
+    private Double price;
+    @Min(5)
+    private String plaque;
+    private String additionalDetails;
+    @Min(5)
+    private String postalCode;
+    private String vehiclType;
+    private String vehicleCondition;
+    private String transmission;
+    private String owner;
+    private Byte[] image;
+    private String vehicleUrl;
+    private EmployeeDTO employeeDTO;
+}
