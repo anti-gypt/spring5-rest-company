c3f22f0988b7dbc28ffcede87c20c312d3a18221
omid-joukar <joukaromid6@gmail.com>
2021-03-01 09:16:26 +0100

    added department products. Closes #60

---
+++ src/main/java/antigypt/springframework/domain/DepartmentProducts.java
@@ -0,0 +1,34 @@
+package antigypt.springframework.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+/**
+ * Created by omid on 11/20/2020.
+ */
+
+@Entity
+@Table(name="DepartmentProducts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentProducts extends Products{
+
+    @OneToOne
+    @JoinColumn(name = "departmentId")
+    private Department department;
+
+
+    //@ManyToMany(cascade = CascadeType.ALL)
+    //@JoinTable(name = "ProductBuyTrolley",
+    //        joinColumns = @JoinColumn(name = "productId"),
+    //        inverseJoinColumns = @JoinColumn(name = "buyTrolleyId"))
+    //public List<BuyTrolley> buyTrolleyList = new ArrayList<>();
+
+}
